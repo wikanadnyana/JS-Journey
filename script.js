755cb29f35a47b2ca362b1dd68b667b8b27fd06a
wikanadnyana <wikanadnyana44@gmail.com>
2022-07-13 22:51:13 +0800

    fyuh

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
+// BELAJAR JAVASCRIPT
+
+/*
+    Materi 1 : 
+    a. querySelector = fungsinya untuk select id pertama pada kode program yang
+    memiliki nama sesuai yang di select.
+
+*/
+
+let a = [1,2,3,4];
+let b = 12
+let result = a[2] * b;
 const heading = document.querySelector('#tes');
-heading.textContent = "Hello Semuanya";
+if(result === 24){
+    heading.textContent = "Hasilnya 24";
+}else{
+    heading.textContent = "Hasilnya bukan 24";
+}
+
